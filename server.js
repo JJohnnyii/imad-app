db80da86e4a225deda2f561c43ba4b37f1e88164
JJohnnyii <30806747+JJohnnyii@users.noreply.github.com>
2017-08-20 22:23:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+/*
 app.get('/cnt1', function (req, res) {
   res.send('trying redirect');
 });
-
+*/
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
